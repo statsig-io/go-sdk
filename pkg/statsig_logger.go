91f54af3c8fd7a5a78257ffc18f3e1e343aad4c6
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-07-20 10:56:51 -0700

    Add exposure logging helpers

--- pkg/statsig_logger.go
+++ pkg/statsig_logger.go
@@ -4,9 +4,12 @@ import (
 	"sync"
 	"statsig/internal/net"
 	"statsig/pkg/types"
+	"strconv"
 )
 
 const MaxEvents = 500
+const GateExposureEvent = "statsig::gate_exposure"
+const ConfigExposureEvent = "statsig::config_exposure"
 
 type statsigLogger struct {
 	events []types.StatsigEvent
@@ -29,6 +32,40 @@ func (l *statsigLogger) Log(evt types.StatsigEvent) {
 	if (len(l.events) >= MaxEvents) {
 		l.flush()
 	}
+}
+
+func (l *statsigLogger) logGateExposure(
+	user types.StatsigUser,
+	gateName string,
+	value bool,
+	ruleID string,
+) {
+	evt := &types.StatsigEvent{
+		User: user,
+		EventName: GateExposureEvent,
+		Metadata: map[string]string{
+			"gate": gateName,
+			"gateValue": strconv.FormatBool(value),
+			"ruleID": ruleID,
+		},
+	}
+	l.Log(*evt)
+}
+
+func (l *statsigLogger) logConfigExposure(
+	user types.StatsigUser,
+	configName string,
+	ruleID string,
+) {
+	evt := &types.StatsigEvent{
+		User: user,
+		EventName: ConfigExposureEvent,
+		Metadata: map[string]string{
+			"config": configName,
+			"ruleID": ruleID,
+		},
+	}
+	l.Log(*evt)
 }
 
 func (l *statsigLogger) flush() {
