26336f7b8bf25529af0fd8126943eb2a8f60a850
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-07-28 13:32:47 -0700

    wait for config specs

--- internal/evaluation/store.go
+++ internal/evaluation/store.go
@@ -95,7 +95,7 @@ func (s *Store) fetchConfigSpecs() DownloadConfigSpecResponse {
 		StatsigMetadata: s.network.GetStatsigMetadata(),
 	}
 	var specs DownloadConfigSpecResponse
-	go s.network.RetryablePostRequest("/download_config_specs", input, &specs, net.SingleRetry)
+	s.network.PostRequest("/download_config_specs", input, &specs)
 	s.lastSyncTime = specs.Time
 	return specs
 }
