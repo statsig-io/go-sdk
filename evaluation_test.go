5c3cc50b4b913f50e2f7edbdf716ad4749330165
Jiakan Wang <jkw@statsig.io>
2021-10-21 17:14:58 -0700

    fix us east test

--- evaluation_test.go
+++ evaluation_test.go
@@ -36,7 +36,7 @@ var secret string
 var testAPIs = []string{
 	"https://api.statsig.com/v1",
 	"https://us-west-2.api.statsig.com/v1",
-	"https://us-east-2.api.statsig.com/v1",
+	"https://az-eastus-2.api.statsig.com/v1",
 	"https://ap-south-1.api.statsig.com/v1",
 	"https://latest.api.statsig.com/v1",
 	"https://az-northeurope.api.statsig.com/v1",
