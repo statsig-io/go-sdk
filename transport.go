bdedae0111154d3276b9b5a989f7599b003cc7b3
Jiakan Wang <jkw@statsig.io>
2022-09-20 11:55:53 -0700

    bump 1.6.0

--- transport.go
+++ transport.go
@@ -47,7 +47,7 @@ func newTransport(secret string, options *Options) *transport {
 
 	return &transport{
 		api:       api,
-		metadata:  statsigMetadata{SDKType: "go-sdk", SDKVersion: "1.5.1"},
+		metadata:  statsigMetadata{SDKType: "go-sdk", SDKVersion: "1.6.0"},
 		sdkKey:    secret,
 		client:    &http.Client{},
 		options:   options,
