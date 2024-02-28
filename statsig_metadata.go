2a61098761070d35b585d9be531e0c85c34c62c5
kenny-statsig <111380336+kenny-statsig@users.noreply.github.com>
2024-02-27 17:38:25 -0800

    [release] 1.18.0 - Persistent storage, ID lists for Data Adapter (#157)
    Added an option to provide a custom storage adapter for user-persisted
    values `IUserPersistentStorage`. This allows the SDK the persist values
    for users in active experiments, in other words, run experiments with
    sticky bucketing.
    
    Added support for syncing ID lists from the data adapter.
    
    
    
    
    
    
    >Included In This Release
    >- 6c7924e2a1a06e313284166db7dfd1aab41017d9 kenny-statsig
    >   - save list of ids to adapter (#156)
    >- a892014b5a16bdfaff5e1a208b5f6dcf9b3ec38d kenny-statsig
    >   - EE-205 support ID lists in data adapter (#154)
    >- 5cd2c39e36d2d6b2030ca2e5bb3a79b9eaef34e8 kenny-statsig
    >   - user persistent storage (#152)
    >- 4f830e40179d8b6799bd0f11fd68bf1453160c64 kenny-statsig
    >   - replace sleep with wait condition in tests (#153)

--- statsig_metadata.go
+++ statsig_metadata.go
@@ -14,7 +14,7 @@ type statsigMetadata struct {
 func getStatsigMetadata() statsigMetadata {
 	return statsigMetadata{
 		SDKType:         "go-sdk",
-		SDKVersion:      "1.17.0",
+		SDKVersion:      "1.18.0",
 		LanguageVersion: runtime.Version()[2:],
 		SessionID:       SessionID(),
 	}
