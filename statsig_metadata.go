4c81d81a1b0171824483840e48057d936f9dd67b
kat-statsig <167801639+kat-statsig@users.noreply.github.com>
2024-06-27 10:29:05 -0700

    [release] v1.25.0 - Sanitize error messages (#196)
    ### Improvements
     - Remove secret keys in output logs
    
    
    
    
    
    >Included In This Release
    >- 65247f2b679fe46b3a7f3d7dea61bf0913767af1 kat-statsig
    >   - sanitize msg in output logger (#195)
    >- e477619421df737b04a727925d4141322e4000d7 kenny-statsig
    >   - fix error interface nil check (#194)
    >- 954c7240c8f64389e0f42e923872005c7629342c kat-statsig
    >   - filter secret from err output (#193)

--- statsig_metadata.go
+++ statsig_metadata.go
@@ -14,7 +14,7 @@ type statsigMetadata struct {
 func getStatsigMetadata() statsigMetadata {
 	return statsigMetadata{
 		SDKType:         "go-sdk",
-		SDKVersion:      "v1.24.0",
+		SDKVersion:      "v1.25.0",
 		LanguageVersion: runtime.Version()[2:],
 		SessionID:       SessionID(),
 	}
