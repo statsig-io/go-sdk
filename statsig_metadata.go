ce7ee60d00936dcca4074d37b5725d96a6833225
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2024-11-26 11:42:46 -0800

    [release] v1.30.3 - Fetch download_config_specs from cdn by default (#245)
    ### Improvements
    - Fetch DCS payload from cdn by default. This will improve reliability
    and latency of initialization and background updates
    
    
    
    
    
    
    >Included In This Release
    >- a105bb0781f702dd1893005d4fc5ad6752ad40d5 Xin Li
    >   - fix: Default hit cdn for dcs (#244)

--- statsig_metadata.go
+++ statsig_metadata.go
@@ -14,7 +14,7 @@ type statsigMetadata struct {
 func getStatsigMetadata() statsigMetadata {
 	return statsigMetadata{
 		SDKType:         "go-sdk",
-		SDKVersion:      "v1.30.2",
+		SDKVersion:      "v1.30.3",
 		LanguageVersion: runtime.Version()[2:],
 		SessionID:       SessionID(),
 	}
