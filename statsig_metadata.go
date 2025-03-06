572570f12a0c3a24aaa496a5fc806c7363325902
kat-statsig <167801639+kat-statsig@users.noreply.github.com>
2025-03-06 12:25:35 -0800

    [release] v1.35.0 - Log StatsigOptions, sampling edge case fix, and add id type to feature gate (#271)
    ### Improvements
      - gate values now include information on id_type
      - send statsigOptions when errors are caught
      ### Fixes
      - fix an edge case in sampling with nested gate
    
    
    
    
    
    >Included In This Release
    >- 2ccc18f0119ef378881ba705764c871a1ebafa3e kat-statsig
    >   - sampling fix (#270)
    >- 84112399121e256f3217fc4c0578869277b2868a kat-statsig
    >   - add statsig option to eb (#269)
    >- 9a2477ac166bef1fe540545485835369da838d59 kat-statsig
    >   - add id type to feature gate (#268)

--- statsig_metadata.go
+++ statsig_metadata.go
@@ -14,7 +14,7 @@ type statsigMetadata struct {
 func getStatsigMetadata() statsigMetadata {
 	return statsigMetadata{
 		SDKType:         "go-sdk",
-		SDKVersion:      "v1.34.1",
+		SDKVersion:      "v1.35.0",
 		LanguageVersion: runtime.Version()[2:],
 		SessionID:       SessionID(),
 	}
