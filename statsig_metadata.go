fb8d78a5d5a2c7100936c711a5fffc8ae4146e37
sroyal-statsig <76536058+sroyal-statsig@users.noreply.github.com>
2025-06-02 14:28:47 -0700

    [release] v1.36.4 - GCIR Options Updates (#288)
    ### Fixes
    - Makes it so local overrides work with the new experiment control
    option for get client initialize response
    
    
    
    
    
    >Included In This Release
    >- 87c594741490158a0c2b741d9c6f3c992ba3b8e0 sroyal-statsig
    >   - Respect Overrides for GCIR Exp Control Option (#287)

--- statsig_metadata.go
+++ statsig_metadata.go
@@ -14,7 +14,7 @@ type statsigMetadata struct {
 func getStatsigMetadata() statsigMetadata {
 	return statsigMetadata{
 		SDKType:         "go-sdk",
-		SDKVersion:      "v1.36.3",
+		SDKVersion:      "v1.36.4",
 		LanguageVersion: runtime.Version()[2:],
 		SessionID:       SessionID(),
 	}
