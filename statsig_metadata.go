b2af1f8a6242924ad5fe9535f06cc8069da5dbc6
kat-statsig <167801639+kat-statsig@users.noreply.github.com>
2025-08-15 15:42:01 -0700

    [release] v1.38.0 - Support for observability client (#298)
    ### New Features
      - new option to pass in an observability client to collect sdk metrics
    
    
    
    
    
    >Included In This Release
    >- 9f618a46cbba76de46ec29cb8b7d583e22526c1e kat-statsig
    >   - add observability client (#297)

--- statsig_metadata.go
+++ statsig_metadata.go
@@ -14,7 +14,7 @@ type statsigMetadata struct {
 func getStatsigMetadata() statsigMetadata {
 	return statsigMetadata{
 		SDKType:         "go-sdk",
-		SDKVersion:      "v1.37.0",
+		SDKVersion:      "v1.38.0",
 		LanguageVersion: runtime.Version()[2:],
 		SessionID:       SessionID(),
 	}
