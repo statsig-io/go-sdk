ac65a87f9de0e94515157c0d52de8179c1e8850c
sroyal-statsig <76536058+sroyal-statsig@users.noreply.github.com>
2025-08-22 15:00:59 -0700

    [release] v1.39.1 - Do not send zero since time in DCS url (#303)
    ### Fixes
    - Removes since time from dcs url when it is 0 because of cloudflare
    issues
    
    
    
    
    
    >Included In This Release
    >- 41befcf1a1dab990363a5d55311b622312576c3f sroyal-statsig
    >   - Remove Since Time From DCS URL When Zero (#302)

--- statsig_metadata.go
+++ statsig_metadata.go
@@ -14,7 +14,7 @@ type statsigMetadata struct {
 func getStatsigMetadata() statsigMetadata {
 	return statsigMetadata{
 		SDKType:         "go-sdk",
-		SDKVersion:      "v1.39.0",
+		SDKVersion:      "v1.39.1",
 		LanguageVersion: runtime.Version()[2:],
 		SessionID:       SessionID(),
 	}
