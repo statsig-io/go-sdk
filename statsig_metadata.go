95e0f4f74ff758108ca96d8824835c208ce679eb
kat-statsig <167801639+kat-statsig@users.noreply.github.com>
2025-09-15 09:33:05 -0700

    [release] v1.40.0 - Support session replay in gcir (#305)
    ### New Features
      - Support session replay in getClientInitializeResponse
    
    
    
    
    
    >Included In This Release
    >- d7013c7d6f06e044c020105f82bfe5347998e74f kat-statsig
    >   - add session replay to gcir (#304)

--- statsig_metadata.go
+++ statsig_metadata.go
@@ -14,7 +14,7 @@ type statsigMetadata struct {
 func getStatsigMetadata() statsigMetadata {
 	return statsigMetadata{
 		SDKType:         "go-sdk",
-		SDKVersion:      "v1.39.1",
+		SDKVersion:      "v1.40.0",
 		LanguageVersion: runtime.Version()[2:],
 		SessionID:       SessionID(),
 	}
